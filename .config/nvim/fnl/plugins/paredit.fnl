aba6f5618f50673d3b8f65a0f621b9e6bf56a5e0
Rafael Delboni <rafadelboni@gmail.com>
2023-10-09 20:59:31 -0300

    feat: adds fnl paredit, surround and auto pair

--- .config/nvim/fnl/plugins/paredit.fnl
+++ .config/nvim/fnl/plugins/paredit.fnl
@@ -6,5 +6,22 @@
   :ft [:clojure :fennel]
   :config (fn []
             (let [paredit (require :nvim-paredit)]
-              (paredit.setup)))
-  }]
+              (paredit.setup)))}
+
+ {1 :julienvincent/nvim-paredit-fennel
+  :dependencies [:julienvincent/nvim-paredit]
+  :lazy true
+  :ft [:fennel]
+  :config (fn []
+            (let [paredit-fnl (require :nvim-paredit-fennel)]
+              (paredit-fnl.setup)))}
+
+ {1 :kylechui/nvim-surround
+  :event "VeryLazy"
+  :config (fn []
+            (let [surround (require :nvim-surround)]
+              (surround.setup)))}
+
+ {1 :windwp/nvim-autopairs
+  :event "InsertEnter"
+  :opts {}}]
--- .config/nvim/lua/plugins/paredit.lua
+++ .config/nvim/lua/plugins/paredit.lua
@@ -6,4 +6,12 @@ local function _2_()
   local paredit = require("nvim-paredit")
   return paredit.setup()
 end
-return {{"julienvincent/nvim-paredit", lazy = true, ft = {"clojure", "fennel"}, config = _2_}}
+local function _3_()
+  local paredit_fnl = require("nvim-paredit-fennel")
+  return paredit_fnl.setup()
+end
+local function _4_()
+  local surround = require("nvim-surround")
+  return surround.setup()
+end
+return {{"julienvincent/nvim-paredit", lazy = true, ft = {"clojure", "fennel"}, config = _2_}, {"julienvincent/nvim-paredit-fennel", dependencies = {"julienvincent/nvim-paredit"}, lazy = true, ft = {"fennel"}, config = _3_}, {"kylechui/nvim-surround", event = "VeryLazy", config = _4_}, {"windwp/nvim-autopairs", event = "InsertEnter", opts = {}}}
